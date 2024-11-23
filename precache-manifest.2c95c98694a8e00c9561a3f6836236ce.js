c8612d77311be3854bbe66139c0d00979ee304fa
Karim <st.herbinar@gmail.com>
2024-11-23 22:38:32 +0100

    Add hamburger menu - make readabillity better at phone

---
+++ precache-manifest.2c95c98694a8e00c9561a3f6836236ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261252766661381263456bf68c8f2e00",
+    "url": "/BondaiKa/index.html"
+  },
+  {
+    "revision": "ffcc52a5db3e96ff1bdf",
+    "url": "/BondaiKa/static/css/main.bb3f72f0.chunk.css"
+  },
+  {
+    "revision": "8d91b0f9b3a22769903b",
+    "url": "/BondaiKa/static/js/2.8f7d27fd.chunk.js"
+  },
+  {
+    "revision": "ffcc52a5db3e96ff1bdf",
+    "url": "/BondaiKa/static/js/main.d6f3dded.chunk.js"
+  },
+  {
+    "revision": "4692a44e2e977d22a617",
+    "url": "/BondaiKa/static/js/runtime~main.5f070dc4.js"
+  },
+  {
+    "revision": "ee05025c156c22376fb84adad6c6f837",
+    "url": "/BondaiKa/static/media/profile.ee05025c.jpg"
+  }
+]);
